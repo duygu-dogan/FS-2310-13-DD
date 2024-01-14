c516bb29f2e52369d805b4fa00f97d65fee7f4b1
duygu-dogan <duygudogan1509@gmail.com>
2024-01-14 14:48:36 +0300

    MiniShop MVC is done, intro to API project

---
+++ Section-09-MVC/Week-13/13-01-2024/MiniShop/MiniShop.Data/Concrete/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using MiniShop.Data.Abstract;
+using MiniShop.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Data.Concrete
+{
+    public class ProductRepository:GenericRepository<Product>,IProductRepository
+    {
+        public ProductRepository(AppDbContext _appDbContext):base(_appDbContext)
+        {
+            
+        }
+
+        AppDbContext AppContext {
+            get 
+            {
+                return _dbContext as AppDbContext;
+            } 
+        }
+
+        public List<Product> GetHomePageProducts(bool? isHome)
+        {
+            var products = AppContext
+                .Products
+                .Where(p => p.IsHome==isHome && p.IsActive && !p.IsDelete)
+                .ToList(); //LINQ (language interface query)-sorgular için arabirim
+            return products;
+        }
+
+        public List<Product> GetProductByCategoryId(int categoryId)
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+}
