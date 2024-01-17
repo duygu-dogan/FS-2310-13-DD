be872dbaea85306ee7ef7b1fb67675acbb2e4e15
duygu-dogan <duygudogan1509@gmail.com>
2024-01-17 21:52:13 +0300

    AutoMapper done

---
+++ Section-09-MVC/Week-14/16-01-2024/MiniShop/MiniShop.Data/Concrete/Contexts/MiniShopDbContext.cs
@@ -0,0 +1,29 @@
+﻿    using Microsoft.EntityFrameworkCore;
+using MiniShop.Data.Concrete.Configs;
+using MiniShop.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Data.Concrete.Contexts
+{
+    public class MiniShopDbContext : DbContext
+    {
+        public MiniShopDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Her projemiz bir assembly, categoryconfig'in olduğu assembly'de aynı tipte olanlara uygula:
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CategoryConfig).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Section-09-MVC/Week-14/17-01-2024/MiniShop/MiniShop.API/Program.cs
@@ -0,0 +1,33 @@
+//Application Program Interface
+using Microsoft.EntityFrameworkCore;
+using MiniShop.Data.Concrete.Contexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<MiniShopDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"))
+    );
+
+
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
