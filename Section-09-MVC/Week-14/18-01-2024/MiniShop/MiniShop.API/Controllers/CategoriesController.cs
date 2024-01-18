51b4614862f2f02210bdb8d95ea92ce3c1178b73
duygu-dogan <duygudogan1509@gmail.com>
2024-01-18 23:38:51 +0300

    GetAllCategoriesWProduct done

---
+++ Section-09-MVC/Week-14/18-01-2024/MiniShop/MiniShop.API/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiniShop.Business.Abstract;
+
+namespace MiniShop.API.Controllers
+{
+    //localhost:5000/api/Categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryManager;
+
+        public CategoriesController(ICategoryService categoryManager)
+        {
+            _categoryManager = categoryManager;
+        }
+
+        [HttpGet] //default olmasına rağmen Swagger istediği için belirtmemiz gerekiyor.
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var response = await _categoryManager.GetAllAsync();
+            return null;
+        }
+    }
+}
---
+++ Section-09-MVC/Week-14/18-01-2024/MiniShop/MiniShop.Shared/ResponseDTOs/Response.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.ResponseDTOs
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public List<string> Errors { get; set; }
+        public int StatusCode { get; set; }
+        public bool IsSucceeded { get; set; }
+
+        //factory design pattern
+
+        /// <summary>
+        /// Bu metot yapılan işlemin başarılı olduğu durumlarda, başarılı bir cevapla birlikte üretilen datayı ve statuscode'u döndürmek için kullanır.
+        /// </summary>
+        /// <param name="data">Geri döndürülecek veri</param>
+        /// <param name="statusCode">İşlemin sonucunu ifade eden kod(200, 201 vb.)</param>
+        /// <returns>Response<typeparamref name="T"/></returns>
+
+        public static Response<T> Success(T data, int statusCode) 
+        { 
+            return new Response<T>
+            { Data = data, StatusCode = statusCode,  IsSucceeded=true };
+        }
+        /// <summary>
+        /// İşlemin başarılı olduğunu ifade eden bir statuscode döndürmek için kullanılır.
+        /// </summary>
+        /// <param name="statusCode">İşlemin sonucunu ifade eden kod.(200, 201 vb)</param>
+        /// <returns>Response<typeparamref name="T"/></returns>
+        public static Response<T> Success(int statusCode) 
+        { 
+            return new Response<T>
+            {
+                Data = default(T), 
+                StatusCode = statusCode,
+                IsSucceeded = true 
+            };
+        }
+        /// <summary>
+        /// Bu metot geriye başarısız bir cevap olarak içinde birden çok hata olan hata listesini döndürmek için kullanılır.
+        /// </summary>
+        /// <param name="errors">Hata listesi</param>
+        /// <param name="statusCode">İşlemin sonucunu ifade eden kod.(200, 201 vb)</param>
+        /// <returns>Response<typeparamref name="T"/></returns>
+        public static Response<T> Fail(List<string> errors, int statusCode)
+        {
+            return new Response<T> 
+            { StatusCode = statusCode, 
+              IsSucceeded = false, 
+              Errors=errors };
+        }
+        /// <summary>
+        /// Bu metot geriye başarısız bir cevap olarak içinde tek hata olan hata listesini döndürmek için kullanılır.
+        /// </summary>
+        /// <param name="error">Hata metni</param>
+        /// <param name="statusCode">İşlemin sonucunu ifade eden kod.(200, 201 vb)</param>
+        /// <returns>Response<typeparamref name="T"/></returns>
+        public static Response<T> Fail(string error, int statusCode) 
+        {
+            return new Response<T>
+            { StatusCode = statusCode,
+                IsSucceeded = false,
+                Errors = new List<string> { error }
+            };
+        }
+
+        
+    }
+}
