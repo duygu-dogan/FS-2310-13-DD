399e7844aeed110ed54525f58fb736baf9e16d68
duygu-dogan <duygudogan1509@gmail.com>
2024-01-28 16:06:38 +0300

    create, edit, harddelete methods done

---
+++ Section-10-API/Week-15/27-01-2024/MiniShopUI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniShop.MVC.Areas.Admin.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace MiniShop.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            Response<List<ProductViewModel>> response = new Response<List<ProductViewModel>>();
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage responseApi = await httpClient.GetAsync("http://localhost:7700/products");
+                string contentResponseApi = await responseApi.Content.ReadAsStringAsync();
+                response = JsonSerializer.Deserialize<Response<List<ProductViewModel>>>(contentResponseApi);
+            }
+            
+            return View(response.Data);
+        }
+        public async Task<IActionResult> UpdateIsHome(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri= new Uri($"http://localhost:7700/products/updateishome/{id}"),
+                Content=new StringContent("",Encoding.UTF8,"application/json")
+            };
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> UpdateIsActive(int id)
+        {
+            HttpClient httpClient= new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri($"http://localhost:7700/products/updateisactive/{id}"),
+                Content = new StringContent("", Encoding.UTF8, "application/json")
+            };
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        //Ürün oluşturma formunu açacak metot, yeni ürün oluşturacak metodu ayrıca yazacağız.
+        public async Task<IActionResult> Create()
+        {
+            Response<List<CategoryViewModel>> response = new Response<List<CategoryViewModel>>();
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage responseApi = await httpClient.GetAsync("http://localhost:7700/categories");
+                string contentResponseApi = await responseApi.Content.ReadAsStringAsync();
+                response = JsonSerializer.Deserialize<Response<List<CategoryViewModel>>>(contentResponseApi);
+            }
+            AddProductViewModel model = new AddProductViewModel
+            {
+                Categories = response.Data
+            };
+
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(AddProductViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Section-10-API/Week-15/27-01-2024/MiniShopUI/Areas/Admin/Models/AddProductViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MiniShop.MVC.Areas.Admin.Models
+{
+    public class AddProductViewModel
+    {
+        [JsonPropertyName("Name")]
+        [DisplayName("Ürün")]
+        public string Name { get; set; }
+
+
+        [DisplayName("Ürün Özellikleri")]
+        [JsonPropertyName("Properties")]
+        public string Properties { get; set; }
+
+
+        [JsonPropertyName("Price")]
+        [DisplayName("Fiyat")]
+        public decimal Price { get; set; }
+
+
+        [JsonPropertyName("ImageUrl")]
+        [DisplayName("Resim")]
+        public string ImageUrl { get; set; }
+
+
+        [JsonPropertyName("Url")]
+        public string Url { get; set; }
+
+
+        [JsonPropertyName("IsActive")]
+        [DisplayName("Aktif Ürün")]
+        public bool IsActive { get; set; }
+
+
+        [JsonPropertyName("IsHome")]
+        [DisplayName("Anasayfa Ürünü")]
+        public bool IsHome { get; set; }
+
+
+        [JsonPropertyName("CategoryIds")]
+        public int[] CategoryId { get; set; }
+
+
+        [JsonPropertyName("Categories")]
+        [DisplayName("Kategoriler")]
+        public List<CategoryViewModel> Categories { get; set; }
+    }
+}
