a0cc73699ddaa7bf93c2331de246a219029c4339
duygu-dogan <duygudogan1509@gmail.com>
2024-02-01 21:59:37 +0300

    ImageUpload done

---
+++ Section-10-API/Week-16/01-02-2024/MVCFileUploadApp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCFileUploadApp.Core;
+using MVCFileUploadApp.Models;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MVCFileUploadApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UploadImage(IFormFile image) 
+        { 
+            if (image == null)
+            {
+                ViewBag.Message = "Resimde bir sorun var, lütfen tekrar deneyiniz.";
+                return View("Index");
+            }
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", image.FileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                image.CopyTo(stream);
+            }
+
+            ViewBag.Message = "Resim başarıyla yüklendi";
+            return View("Index");
+        }
+
+        [HttpGet]
+        public IActionResult ResimYukle() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ResimYukle(IFormFile image)
+        {
+            var imageUrl = Uploader.UploadImage(image);
+            return View();
+        }
+    }
+}
