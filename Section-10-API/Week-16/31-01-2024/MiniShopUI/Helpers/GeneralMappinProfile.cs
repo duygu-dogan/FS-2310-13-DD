9fa7af19f62a557021e50601abf170274aca8ad9
duygu-dogan <duygudogan1509@gmail.com>
2024-02-01 17:43:34 +0300

    ImageUploader continue

---
+++ Section-10-API/Week-16/31-01-2024/MiniShopUI/Helpers/GeneralMappinProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MiniShop.MVC.Areas.Admin.Models;
+
+namespace MiniShop.MVC.Helpers
+{
+    public class GeneralMappinProfile : Profile
+    {
+        public GeneralMappinProfile()
+        {
+            CreateMap<ProductViewModel, EditProductViewModel>().ReverseMap();
+            CreateMap<ProductViewModel, DeleteProductViewModel>().ReverseMap();
+        }
+
+    }
+}
