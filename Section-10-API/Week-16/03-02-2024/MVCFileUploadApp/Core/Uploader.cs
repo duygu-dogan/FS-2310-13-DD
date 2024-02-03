a0d14d1ebe113cf9a66059fd7d50fcb1c7770cc6
duygu-dogan <duygudogan1509@gmail.com>
2024-02-03 14:48:48 +0300

    image upload done

---
+++ Section-10-API/Week-16/03-02-2024/MVCFileUploadApp/Core/Uploader.cs
@@ -0,0 +1,33 @@
+﻿namespace MVCFileUploadApp.Core
+{
+    public static class Uploader
+    {
+        public static string UploadImage(IFormFile image)
+        {
+            //dosyaismi.uzantı --uzantıyı almak için:[manzara, kadıkoy,png]
+            //manzara.kadıkoy.png = [manzara, kadıkoy,png]
+            //örnek image ismi: iphone12.png
+
+            //.png
+            var extension = "." + image.FileName.Split('.')[image.FileName.Split('.').Length -1];
+
+            //localhost:5000/wwwwroot/images
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+            if (!Directory.Exists(filePath)) 
+            { 
+                Directory.CreateDirectory(filePath);
+            }
+
+            //asdnjffjnkf.png
+            var fileName = $"{Guid.NewGuid()}, {extension}";
+            //localhost:5000/wwwwroot/images/asdnjffjnkf.png
+            var path = Path.Combine(filePath, fileName);
+
+            using(var stream = new FileStream(path, FileMode.Create))
+            {
+                image.CopyTo(stream);
+            }
+            return fileName;
+        }
+    }
+}
---
+++ Section-10-API/Week-16/03-02-2024/MiniShopUI/Areas/Admin/Models/AddProductViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MiniShop.MVC.Areas.Admin.Models
+{
+    public class AddProductViewModel
+    {
+        [JsonPropertyName("Name")]
+        [DisplayName("Ürün")]
+        [Required(ErrorMessage = "{0} alanı boş bırakılamaz.")]
+        [MinLength(5, ErrorMessage ="{0} alanına uzunluğu {1} karakterden az değer girilemez.")]
+        [MaxLength(100, ErrorMessage ="{0} alanına uzunluğu {1} karakterden fazla değer girilemez.")]
+        public string Name { get; set; }
+
+
+        [DisplayName("Ürün Özellikleri")]
+        [JsonPropertyName("Properties")]
+        //[Required(ErrorMessage = "{0} alanı boş bırakılamaz!")]
+        //[MinLength(25, ErrorMessage = "{0} alanına uzunluğu {1} karakterden az değer girilemez.")]
+        public string Properties { get; set; }
+
+
+        [JsonPropertyName("Price")]
+        [DisplayName("Fiyat")]
+        [Required(ErrorMessage = "{0} alanı boş bırakılamaz!")]
+        public decimal? Price { get; set; }
+
+
+        [JsonPropertyName("ImageUrl")]
+        [DisplayName("Resim")]
+        public string ImageUrl { get; set; } = "";
+
+
+        [JsonPropertyName("Url")]
+        public string Url { get; set; }
+
+
+        [JsonPropertyName("IsActive")]
+        [DisplayName("Aktif Ürün")]
+        public bool IsActive { get; set; }
+
+
+        [JsonPropertyName("IsHome")]
+        [DisplayName("Anasayfa Ürünü")]
+        public bool IsHome { get; set; }
+
+
+        [JsonPropertyName("CategoryIds")]
+        public List<int> CategoryIds { get; set; } = new List<int>();
+
+
+        [JsonPropertyName("Categories")]
+        [DisplayName("Kategoriler")]
+        public List<CategoryViewModel> Categories { get; set; }
+    }
+}
