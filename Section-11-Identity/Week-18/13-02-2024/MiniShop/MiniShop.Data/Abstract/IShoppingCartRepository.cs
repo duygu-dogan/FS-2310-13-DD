51cff8734661b65c6be786650a7c2d3b1b9ac391
duygu-dogan <duygudogan1509@gmail.com>
2024-02-13 22:02:22 +0300

    ShoppingCartManager continue

---
+++ Section-11-Identity/Week-18/13-02-2024/MiniShop/MiniShop.Data/Abstract/IShoppingCartRepository.cs
@@ -0,0 +1,17 @@
+﻿using MiniShop.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Data.Abstract
+{
+    public interface IShoppingCartRepository: IGenericRepository<ShoppingCart>
+    {
+        Task<ShoppingCart> GetShoppingCartByUserIdAsync(string id);
+        Task DeleteFromShoppingCartAsync(int shoppingCartId, int productId);
+        Task ClearFromShoppingCartAsync(int shoppingCartId);
+
+    }
+}
