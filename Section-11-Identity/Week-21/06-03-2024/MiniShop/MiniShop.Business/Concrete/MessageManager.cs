ac617718b4d2d5b42abbfd4402dbf73cccd9bfbd
duygu-dogan <duygudogan1509@gmail.com>
2024-03-06 21:55:19 +0300

    MessageService Continue

---
+++ Section-11-Identity/Week-21/06-03-2024/MiniShop/MiniShop.Business/Concrete/MessageManager.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using MiniShop.Business.Abstract;
+using MiniShop.Data.Abstract;
+using MiniShop.Entity.Concrete;
+using MiniShop.Shared.ResponseViewModels;
+using MiniShop.Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Business.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        private readonly IMapper _mapper;
+        private readonly IMessageRepository _messageRepository;
+
+        public MessageManager(IMapper mapper, IMessageRepository messageRepository)
+        {
+            _mapper = mapper;
+            _messageRepository = messageRepository;
+        }
+
+        public async Task<Response<MessageViewModel>> CreateAsync(MessageViewModel messageViewModel)
+        {
+            var createdMessage =  await _messageRepository.CreateAsync(_mapper.Map<Message>(messageViewModel));
+            if (createdMessage == null)
+            {
+                return Response<MessageViewModel>.Fail("Mesaj oluşturulamadı");
+            }
+            var message = _mapper.Map<MessageViewModel>(createdMessage);
+            return Response<MessageViewModel>.Success(message);
+            
+        }
+
+        public async Task<Response<List<MessageViewModel>>> GetAllSentMessagesAsync(string userId)
+        {
+            var messageList = await _messageRepository.GetAllAsync(x => x.SendFromId == userId);
+            if(messageList == null)
+            {
+                return Response<List<MessageViewModel>>.Fail("Mesaj kutusu boş");
+            }
+            var messageViewModelList = _mapper.Map<List<MessageViewModel>>(messageList);
+            return Response<List<MessageViewModel>>.Success(messageViewModelList);
+        }
+        public async Task<Response<List<MessageViewModel>>> GetAllReceivedMessagesAsync(string userId, bool isRead)
+        {
+            var messageList = await _messageRepository.GetAllAsync(x => x.SendToId == userId && x.IsRead == isRead);
+            if (messageList == null)
+            {
+                var errorMessage = !isRead ? "Okunmamış mesaj bulunamadı" : "Okunmuş mesaj bulunamadı";
+                return Response<List<MessageViewModel>>.Fail($"{errorMessage}");
+            }
+            var messageViewModelList = _mapper.Map<List<MessageViewModel>>(messageList);
+            return Response<List<MessageViewModel>>.Success(messageViewModelList);
+        }
+        public async Task<Response<MessageViewModel>> GetByIdAsync(int id)
+        {
+            var message = await _messageRepository.GetByIdAsync(x => x.Id == id);
+            if (message == null)
+            {
+                return Response<MessageViewModel>.Fail("Mesaj bulunamadı");
+            }
+            var messageViewModel = _mapper.Map<MessageViewModel>(message);
+            return Response<MessageViewModel>.Success(messageViewModel);
+        }
+
+        public async Task<Response<int>> GetMessageCountAsync(string userId, bool isRead = false)
+        {
+            var messageCount = await _messageRepository.GetCount(x => x.SendFromId == userId && x.IsRead == isRead);
+            if (messageCount == 0)
+            {
+                return Response<int>.Fail("Mesaj bulunamadı");
+            }
+            return Response<int>.Success(messageCount);
+        }
+
+        public async Task<Response<NoContent>> HardDeleteAsync(int id)
+        {
+            var message = await _messageRepository.GetByIdAsync(x => x.Id == id);
+            await _messageRepository.HardDeleteAsync(message);
+            return Response<NoContent>.Success();
+        }
+
+        public async Task<Response<NoContent>> MarkAsReadAsync(int id)
+        {
+            var message = await _messageRepository.GetByIdAsync(x => x.Id == id);
+            message.IsRead = true;
+            await _messageRepository.UpdateAsync(message);
+            return Response<NoContent>.Success();
+        }
+    }
+}
---
+++ Section-11-Identity/Week-21/06-03-2024/MiniShop/MiniShop.Shared/ViewModels/MessageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MiniShop.Shared.ViewModels.IdentityModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.ViewModels
+{
+    public class MessageViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Mesaj: ")]
+        [Required(ErrorMessage = "Mesaj alanı boş geçilemez")]
+        public string Text { get; set; }
+        public string SendToId { get; set; }
+        public string SendFromId { get; set; }
+        public bool IsRead { get; set; }
+        [DisplayName("Gönderim Tarihi: ")]
+        public DateTime SendDate { get; set; } = DateTime.Now;
+
+        [DisplayName("Kime: ")]
+        public string SendToName { get; set; }
+        [DisplayName("Kimden: ")]
+        public string SendFromName { get; set; }
+        public List<SelectListItem> UserList { get; set; }
+
+    }
+}
